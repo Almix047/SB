ec200b2ba81fd34b18afa15ebe504365437e648b
Almix047 <Amix047@gmail.com>
2020-10-08 16:57:19 +0300

    Add Webdriver

--- app.rb
+++ app.rb
@@ -5,7 +5,12 @@ require_relative 'input_list_fetcher.rb'
 
 INPUT = InputListFetcher.read_from_file
 
-# DRIVER = Selenium::WebDriver.for :firefox
+HOST = 'https://socialblade.com/instagram/user/'
+
+options = Selenium::WebDriver::Firefox::Options.new
+options.profile = 'my-profile'
+
+DRIVER = Selenium::WebDriver.for :firefox, options: options
 
 RecordToFile.save_as_csv_file
 
